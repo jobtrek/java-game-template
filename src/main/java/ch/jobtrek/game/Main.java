6a9c5ac4e12f9af7c3eb8c43a93db38773fd0641
fuzoh <info@bastiennicoud.ch>
2026-03-02 15:08:47 +0100

    feat: character task
    closes: #4

--- src/main/java/ch/jobtrek/game/Main.java
+++ src/main/java/ch/jobtrek/game/Main.java
@@ -1,11 +1,26 @@
 package ch.jobtrek.game;
+import ch.jobtrek.game.characters.Character;
 
 
 /**
  * Java main entrypoint class
  */
 public class Main {
-    static void main() {
-        System.out.println("Hello World! Let's build a game with OOP.");
+    public static void main(String[] args) {
+        Character hero = new Character("Hero", 100, 15, 10, 8);
+        System.out.println("Testing encapsulation in Character class:");
+        System.out.println("Initial status: " + hero);
+        
+        System.out.println("Taking 20 damage...");
+        hero.takeDamage(20);
+        System.out.println("Current status: " + hero);
+        
+        System.out.println("Taking 100 damage...");
+        hero.takeDamage(100);
+        System.out.println("Current status: " + hero);
+        
+        System.out.println("Trying to heal 500 hp...");
+        hero.setHp(500);
+        System.out.println("Current status: " + hero);
     }
 }
---
+++ src/main/java/ch/jobtrek/game/characters/Character.java
@@ -0,0 +1,67 @@
+package ch.jobtrek.game.characters;
+
+public class Character {
+    private String name;
+    private int hp;
+    private int maxHp;
+    private int power;
+    private int defense;
+    private int speed;
+
+    public Character(String name, int maxHp, int power, int defense, int speed) {
+        this.name = name;
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+        this.power = power;
+        this.defense = defense;
+        this.speed = speed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public int getMaxHp() {
+        return maxHp;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setHp(int hp) {
+        if (hp < 0) {
+            this.hp = 0;
+        } else if (hp > maxHp) {
+            this.hp = maxHp;
+        } else {
+            this.hp = hp;
+        }
+    }
+
+    public void takeDamage(int amount) {
+        setHp(this.hp - amount);
+    }
+
+    public boolean isAlive() {
+        return hp > 0;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s [HP: %d/%d, POW: %d, DEF: %d, SPD: %d]",
+                             name, hp, maxHp, power, defense, speed);
+    }
+}
