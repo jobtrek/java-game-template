d7de1f38a53bf7804e22bbc4b70aa382a32844af
fuzoh <info@bastiennicoud.ch>
2026-03-02 13:46:32 +0100

    chore: add run task in gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
     id("java")
+    id("application")
 }
 
 group = "ch.jobtrek.game"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "ch.jobtrek.game.Main"
+}
 
 repositories {
     mavenCentral()
